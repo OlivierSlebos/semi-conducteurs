45d9ff86746d2636d79c1630842d3ba6c21a5688
Olivier Slebos <olivierslebos@sp-eduroam-203-162.wireless.uva.nl>
2025-01-07 11:41:08 +0100

    inladen van stations in Kaart

--- RailNL.py
+++ RailNL.py
@@ -5,20 +5,23 @@ class Kaart():
     def __init__(self):
 
         #verbinding station en ID
-        self.stations: dict[int, Station]
+        self.stations: dict[str, Station] = {}
 
         #Laad de stations in
         self.load_stations("station.csv")
 
-    def load_rooms(self, filename: str) -> None:
+    def load_stations(self, filename: str) -> None:
+
+        id = 1
 
         #open document
         with open(filename) as f:
             line = f.readline()
             
             #Maak stations
-            while line != '\n':
-                room_data = line.strip().split("\t")
-                new_room = Room(int(room_data[0]), room_data[1], room_data[2])
-                self.rooms[int(room_data[0])] = new_room
+            while line != "":
+                station_data = line.split(',')
+                nieuw_station = Station(id, station_data[0], station_data[1], station_data[2])
+                self.stations[station_data[0]] = nieuw_station
+                id += 1
                 line = f.readline()
