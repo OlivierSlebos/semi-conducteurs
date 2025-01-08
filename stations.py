702f0dd6e4a4fa4a8b4ed7b2bae930694ab3be03
DaafL6 <180194751+DaafL6@users.noreply.github.com>
2025-01-08 15:04:21 +0100

    7 treinen rijden random routes, beetje afhankelijk van connecties die wel/niet zijn bereden

--- stations.py
+++ stations.py
@@ -8,11 +8,21 @@ class Station:
         self.coordinates_x: str = station_coordinate_x
         self.coordinates_y: str = station_coordinate_y
         self.visited = False
-        self.connections: dict[str, Station] = {}
+        self.connections: dict[str, tuple[Self, int, bool]] = {}
+        self.connection_amount = 0
 
     def add_connection(self, other_station: Self, reisduur: int) -> None:
         # Key = de naam van het doel staion     tuple(Object van het doel station, reisduur)
-        self.connections[other_station.name] = (other_station, reisduur)
+        self.connections[other_station.name] = (other_station, reisduur, False)
+
+    # hij zet hier de connectie tussen de stations op bereden, dit zie je aan de true statement in de tuple, ook connectie amount met -1
+    def set_connection_visited(self, other_station: Self, reisduur) -> None:
+        self.connections[other_station.name] = (other_station, reisduur, True)
+        self.connection_amount -= 1
+
+    # check of de connectie is bereden, wordt uit de tuple gelezen 
+    def is_connection_visited(self, other_station: Self) -> bool:
+        return self.connections[other_station.name][2]
 
     def has_connection(self, direction: str) -> bool:
         if direction in self.connections:
@@ -28,3 +38,8 @@ class Station:
 
     def is_visited(self) -> bool:
         return self.visited
+    
+    # is nodig om aan het begin 1 keer te bepalen met hoeveel connecties een station begint
+    def set_connection_amount(self) -> None:
+        self.connection_amount = len(self.connections)
+    
