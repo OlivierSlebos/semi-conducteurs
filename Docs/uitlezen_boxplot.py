0742fc94d539fe889ddd18116db56d00d2b2f31b
DaafL6 <180194751+DaafL6@users.noreply.github.com>
2025-01-28 12:22:28 +0100

    uitlezen en plotten opgeschoond en comments

--- Docs/uitlezen_boxplot.py
+++ Docs/uitlezen_boxplot.py
@@ -1,22 +1,23 @@
 import csv
 import os
 
+#script voor het uitlezen van de resultaten folder om later een boxplot van te kunnen maken 
 def scores_boxplot_csv() -> None:
 
+    #hoe moet de output heten 
     bestandsnaam = 'scores_boxplot.csv'
     
-    # Lijst van bestanden in de map ophalen
-    bestanden = os.listdir("resultaten")
+    #lijst van bestanden in de map maken
+    bestanden = os.listdir("Resultaten")
     
-    # Alleen bestanden selecteren, geen mappen
-    bestanden = [bestand for bestand in bestanden if os.path.isfile(os.path.join("resultaten", bestand))]
+    #alleen bestanden selecteren, geen mappen
+    bestanden = [bestand for bestand in bestanden if os.path.isfile(os.path.join("Resultaten", bestand))]
     
-    # CSV schrijven
+    #csv schrijven
     with open(fr"Docs/{bestandsnaam}", mode='w', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
         
-        
-        # Bestandnamen en nummers schrijven
+        #aantal treinen en bijbehorende score opschrijven
         for i, bestand in enumerate(bestanden):
             score = bestand.split('_')
             writer.writerow([score[1], score[2]])
