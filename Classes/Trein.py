17739a1df3e9cf95c2ca1e08afcdb5dcb0b75cf4
Daankempen <Daankempen13@gmail.com>
2025-01-29 17:33:14 +0100

    docstrings geschreven voor trein.py

--- Classes/Trein.py
+++ Classes/Trein.py
@@ -4,9 +4,19 @@ from Classes.History import History
 
 class Trein():
     def __init__(self, station):
+        """
+        Initialiseert een trein op een gegeven startstation.
+
+        station (Station): Het station waar de trein begint.
+        """
         self.current_station = station
         self.time_driven = 0
         self.traject_history = History()
 
     def change_station(self, station: Station) -> None:
+        """
+        Verandert het huidige station van de trein.
+
+        station (Station): Het nieuwe station waar de trein naartoe gaat.
+        """
         self.current_station = station
